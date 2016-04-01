75c678fd7c638ab23b23f2281ed30a3eb0b1b6c9
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2016-04-01 18:08:56 +0200

    Set a name to a test run and show a list of stats.

--- lib/specmagick/cli/run.rb
+++ lib/specmagick/cli/run.rb
@@ -5,7 +5,59 @@ module Specmagick
       include TaggedTests
 
       def execute
-        RSpec::Core::Runner.run(computed_args)
+        if listing_latest?
+          list_latest
+        elsif listing_named?
+          list_named
+        elsif setting_name?
+          set_name
+        else
+          RSpec::Core::Runner.run(computed_args)
+        end
+      end
+
+      private
+
+      def latest_scope
+        Specmagick::Models::TestRun.order(Sequel.desc(:created_at)).limit(10)
+      end
+
+      def named_scope
+        latest_scope.where(Sequel.~(name: nil))
+      end
+
+      def list_latest
+        list(latest_scope)
+      end
+
+      def list_named
+        list(named_scope)
+      end
+
+      def list(scope)
+        tf    = "%Y-%m-%d %H:%M"
+        data  = scope.map { |r| [ r.id, r.created_at.strftime(tf), r.name, r.outcomes.count, r.failures.count ] }.reverse
+        table = TTY::Table.new([ 'id', 'time', 'name', 'tests', 'fails' ], data)
+        puts table.render(:ascii, alignments: [ :right, :right, :left, :right, :right ])
+      end
+
+      def set_name
+        (id, name) = command_options[:set_name].split(':', 2)
+        run = id == 'last' ? Specmagick::Models::TestRun.last : Specmagick::Models::TestRun.with_pk!(id)
+        run.name = name
+        run.save
+      end
+
+      def listing_latest?
+        command_options[:list_latest_given]
+      end
+
+      def listing_named?
+        command_options[:list_named_given]
+      end
+
+      def setting_name?
+        command_options[:set_name_given]
       end
 
       def computed_args
--- lib/specmagick/models/test_run.rb
+++ lib/specmagick/models/test_run.rb
@@ -3,6 +3,12 @@ module Specmagick
     class TestRun < Sequel::Model
       plugin :timestamps, create: :created_at
       one_to_many :outcomes, class: :'Specmagick::Models::TestOutcome', key: :run_id
+      one_to_many :successes, class: :'Specmagick::Models::TestOutcome', key: :run_id do |outc|
+        outc.where { success =~ true }
+      end
+      one_to_many :failures, class: :'Specmagick::Models::TestOutcome', key: :run_id do |outc|
+        outc.where { success =~ false }
+      end
       many_to_many :tests
     end
   end
