eab2decd01bd08130511ae9983ac9ea273182090
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2016-04-01 17:22:38 +0200

    Fix association.

--- lib/specmagick/models/test_run.rb
+++ lib/specmagick/models/test_run.rb
@@ -2,7 +2,7 @@ module Specmagick
   module Models
     class TestRun < Sequel::Model
       plugin :timestamps, create: :created_at
-      one_to_many :outcomes, class: :TestOutcome
+      one_to_many :outcomes, class: :'Specmagick::Models::TestOutcome', key: :run_id
       many_to_many :tests
     end
   end
